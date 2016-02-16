431c7a5fede81866c9215bed7aa6ab293973c7a8
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-16 14:52:17 -0800

    Made Bubble Sorting actually Bubble Sorting

--- Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Bubble.java
+++ Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Bubble.java
@@ -13,12 +13,12 @@ public class Bubble {
 	
 	public static void bubbleSort(int[] array) {
 		for(int j = 0; j < array.length - 1; j++) {
-			for(int i = 1; i < array.length; i++) {
+			for(int i = 0; i < array.length - 1; i++) {
 				int index1 = i;
-				int index2 = i - 1;
+				int index2 = i + 1;
 				int temp1 = array[index1];
 				int temp2 = array[index2];
-				if(temp2 > temp1) {
+				if(temp2 < temp1) {
 					array[index1] = temp2;
 					array[index2] = temp1;
 				}
