01d6f8dddc532cf95ec475c9c3f0fb251b674050
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-16 14:30:28 -0800

    Changed to a void

--- Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Bubble.java
+++ Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Bubble.java
@@ -7,28 +7,23 @@ public class Bubble {
 			5, 1, 12, -5, 16
 		};
 		System.out.println(SortingConstants.intArrayToString(array));
-		array = bubbleSort(array);
+		bubbleSort(array);
 		System.out.println(SortingConstants.intArrayToString(array));
 	}
 	
-	public static int[] bubbleSort(int[] array) {
-		int[] tempArray = new int[array.length];
-		for(int i = 0; i < array.length; i++) {
-			tempArray[i] = array[i];
-		}
-		for(int j = 0; j < tempArray.length - 1; j++) {
-			for(int i = 1; i < tempArray.length; i++) {
+	public static void bubbleSort(int[] array) {
+		for(int j = 0; j < array.length - 1; j++) {
+			for(int i = 1; i < array.length; i++) {
 				int index1 = i;
 				int index2 = i - 1;
-				int temp1 = tempArray[index1];
-				int temp2 = tempArray[index2];
+				int temp1 = array[index1];
+				int temp2 = array[index2];
 				if(temp2 > temp1) {
-					tempArray[index1] = temp2;
-					tempArray[index2] = temp1;
+					array[index1] = temp2;
+					array[index2] = temp1;
 				}
 			}
 		}
-		return tempArray;
 	}
 
 }
