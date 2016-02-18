4b8ac5320ddcd867b8e7f19626d95f0491cc8fe4
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-18 13:34:26 -0800

    Insertion Comments

--- Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Insertion.java
+++ Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Insertion.java
@@ -1,27 +1,41 @@
 package com.sshsgd.tut19;
-
+/**
+ * Bubble sorting read left to right, and checked the value after the current value. Insertion
+ * sorting works the same way, but checks the value before the current value instead. If the 
+ * value at the index is greater than the current value, then swap the elements in the list.
+ *
+ */
 public class Insertion {
 
 	public static void main(String[] args) {
-		int array[] = {
+		int array[] = { //Unordered int array
 			5, 2, 4, 6, 1, 3
 		};
-		System.out.println(SortingConstants.intArrayToString(array));
-		insertionSort(array);
-		System.out.println(SortingConstants.intArrayToString(array));
+		System.out.println(SortingConstants.intArrayToString(array)); //Show the array before sort
+		insertionSort(array); //Sort the array
+		System.out.println(SortingConstants.intArrayToString(array)); //Show the array after sort
 	}
 
 	public static void insertionSort(int array[]) {
-		for(int i = 0; i < array.length - 1; i++) {
-			for(int j = 1; j < array.length; j++) {
-				int tempJ = array[j];
-				int tempJM = array[j - 1];
-				if(tempJM > tempJ) {
-					array[j] = tempJM;
-					array[j - 1] = tempJ;
+		for(int i = 0; i < array.length - 1; i++) { //Go through the array 1 time less than the length
+			for(int j = 1; j < array.length; j++) { //Starting at the second index, loop through the array
+				int tempJ = array[j]; //Current value
+				int tempJM = array[j - 1]; //Value before current
+				if(tempJM > tempJ) { //If the previous number is greater than the current number
+					array[j] = tempJM; //Set the number at the current index to the previous number
+					array[j - 1] = tempJ; //Set the number at the previous index to the original number
 				}
 			}
 		}
 	}
+	
+	/**
+	 * 
+	 * EXPECTED OUTPUT:
+	 * 
+	 * [5, 2, 4, 6, 1, 3]
+	 * [1, 2, 3, 4, 5, 6]
+	 * 
+	 */
 	
 }
