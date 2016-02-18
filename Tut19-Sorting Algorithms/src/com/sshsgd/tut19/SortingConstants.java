180845f834ed81ec50203864f1edc0dd55f48991
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-18 13:22:12 -0800

    Sorting Constants Comments

--- Tut19-Sorting Algorithms/src/com/sshsgd/tut19/SortingConstants.java
+++ Tut19-Sorting Algorithms/src/com/sshsgd/tut19/SortingConstants.java
@@ -3,14 +3,14 @@ package com.sshsgd.tut19;
 public class SortingConstants {
 
 	public static String intArrayToString(int[] array) {
-		String s = "[";
-		for(int i = 0; i < array.length; i++) {
-			s += array[i];
+		String s = "["; //Start the String with an opening square bracket
+		for(int i = 0; i < array.length; i++) { //Loop through each element of the array
+			s += array[i]; //Add the value to the string
 			if(i < array.length - 1) {
-				s += ", ";
+				s += ", "; //If the current element is not the last, add a comma and a space after the value
 			}
 		}
-		s += "]";
+		s += "]"; //Close the String with a closing square bracket
 		return s;
 	}
 	
