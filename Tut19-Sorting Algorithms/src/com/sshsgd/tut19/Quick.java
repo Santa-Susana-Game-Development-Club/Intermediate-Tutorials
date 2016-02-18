09fcd5890b24262c7afef8cbd5406deec797f70f
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-18 13:42:26 -0800

    Quick Comments

--- Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Quick.java
+++ Tut19-Sorting Algorithms/src/com/sshsgd/tut19/Quick.java
@@ -1,5 +1,14 @@
 package com.sshsgd.tut19;
 
+/**
+ * Quick sorting is probably the best sorting method. It may seem more complex, but is 
+ * tremendously faster and more efficient than the others. It works by choosing a pivot, which
+ * is any element in the array, and moving all the values less than the pivot to be before
+ * it, and the values greater come after it. It essentially makes two new arrays and recursively
+ * sorts each of them until the array is completely sorted.
+ *
+ */
+
 public class Quick {
 
 	public static void main(String[] args) {
@@ -9,8 +18,6 @@ public class Quick {
 		System.out.println(SortingConstants.intArrayToString(array));
 		quickSort(array, 0, array.length - 1);
 		System.out.println(SortingConstants.intArrayToString(array));
-		
-
 	}
 	
 	public static void quickSort(int[] array, int low, int high) {
@@ -44,5 +51,14 @@ public class Quick {
 			quickSort(array, i, high);
 		}
 	}
+	
+	/**
+	 * 
+	 * EXPECTED OUTPUT:
+	 * 
+	 * [3, 7, 8, 5, 2, 1, 9, 5, 4]
+	 * [1, 2, 3, 4, 5, 5, 7, 8, 9]
+	 * 
+	 */
 
 }
